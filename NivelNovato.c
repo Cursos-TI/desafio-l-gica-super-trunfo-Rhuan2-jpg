0ea9746292eb08cebcfb2220e5d052a3bfa37a68
Rhuan2-jpg <rhuantavares2020@gmail.com>
2025-06-04 13:28:21 +0000

    Completo

--- NivelNovato.c
+++ NivelNovato.c
@@ -7,7 +7,9 @@
     int PontosTuristicos;
     float area, pib;
     int temp_char;
-       //Explicação do que deve ser inserido na primeira carta
+    int divisao, divisao1; 
+    int divisao2, divisao3; 
+        //Explicação do que deve ser inserido na primeira carta
       printf("Insira os dados das duas cartas, a letra do estado deve ser de A a H, a Área deve ser em km², o código da carta deve ser de 01 a 04 e deve estar depois da letra do estado. A = Acre, B = Bahia, C = Ceará, D = Espírito Santo, E = Goiás, F = Maranhão, G = Alagoas, H = Amazonas, Depois escolha uma cidade, informe o número de habitantes da cidade, informe a área da cidade, informe o PIB, informe o Número de pontos turísticos.\n");
       printf("Escolha seu estado: ");
       scanf(" %c", &estado);
@@ -36,10 +38,15 @@
       printf("Informe o Número de Pontos Turísticos: ");
       scanf("%d", &PontosTuristicos);
       while ((temp_char = getchar()) != '\n' && temp_char != EOF);
+       //divisão da densidade populacional e do pib per capita 
+       divisao = Populacao / area;
+       printf("A Densidade populacional é: %d\n", divisao);
         
-       
+       divisao1 = pib / Populacao;
+       printf("O PIB per capita é : %d\n", divisao1);
+        
         //Informações da primeira carta juntas
-       printf("as informações da sua carta são estas respectivamente: %c, %s, %s, %d, %.2f km², %.2f, %d\n", estado, CodigoDaCarta, NomeDaCidade, Populacao, area, pib, PontosTuristicos);
+       printf("as informações da sua carta são estas respectivamente: Estado %c, Código %s, Cidade %s, População %d, Área %.2f km², PIB %.2f, Pontos Turísticos %d, Densidade Populacional %d, PIB per capita %d\n", estado, CodigoDaCarta, NomeDaCidade, Populacao, area, pib, PontosTuristicos, divisao, divisao1);
         //Dados e váriaveis para segunda carta
     char Estado;
     char codigoDaCarta[4],nomeDaCidade[25]; 
@@ -74,8 +81,18 @@
       printf("Informe o Número de Pontos Turísticos: ");
       scanf("%d", &pontosTuristicos);
       while ((temp_char = getchar()) != '\n' && temp_char != EOF);
+       
+       //divisão da densidade populacional e do pib per capita 
+       divisao2 = populacao / Area;
+       printf("A Densidade populacional é: %d\n", divisao2);
+        
+       divisao3 = Pib / populacao;
+       printf("O PIB per capita é : %d\n", divisao3);
+      
         //Informações da segunda carta juntas
-       printf("as informações da sua carta são estas respectivamente: %c, %s, %s, %d, %.2f km², %.2f, %d\n", Estado, codigoDaCarta, nomeDaCidade, populacao, Area, Pib, pontosTuristicos);
+       printf("as informações da sua carta são estas respectivamente: Estado %c, Código %s, Cidade %s, População %d, Área %.2f km², PIB %.2f, Pontos Turísticos %d, Densidade Populacional %d, PIB per capita %d\n", Estado, codigoDaCarta, nomeDaCidade, populacao, Area, Pib, pontosTuristicos, divisao2, divisao3);
      
-    
+        if(pib > Pib){
+          printf("A Carta 1 venceu, %c, %.2f", Estado, pib);
+        } else { printf("A Carta 2 venceu, %c, %.2f", estado, Pib); }
     }
